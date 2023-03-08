315a63cd4fc15a20b62e1d842b6d51683078a673
SaravanarajuR <SaravanarajuR>
2023-03-08 21:21:52 +0530

    small update

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-//closure
+////////////////////////////////////////////CLOSURE/////////////////////////////////////////////
+function closure(a) {
+  return function inner(b) {
+    console.log(a);
+    console.log(b);
+  };
+}
 
-//function currying
+const closureStore = closure("n");
+closureStore("x");
+closureStore("p");
 
-function fun(a) {
+//////////////////////////////////////FUNCTION CURRYING///////////////////////////////////////
+//1
+
+function curry(a) {
   return function (b) {
-    if (b != null) return fun(a + b);
+    return function (c) {
+      return Math.floor((a * b) / c);
+    };
+  };
+}
+
+console.log(curry(10)(20)(2));
+
+//2
+
+function infCurry(a) {
+  return function (b) {
+    if (b != null) return infCurry(a + b);
     return a;
   };
 }
 
-console.log(fun(10)(20)(30)(80)(56)(34)());
+console.log(infCurry(10)(20)(30)(80)(56)(34)());
+
+////////////////////////////PARTIAL APPLICATION OF FUNCTIONS////////////////////////////////////
